a1688975f2cf4105f3484db6a7b3af3b127f9069
Julio Napurí <julionc@gmail.com>
2013-12-02 17:25:17 -0500

    Detect if rbenv is installed

--- zsh/conf.d/environment.zsh
+++ zsh/conf.d/environment.zsh
@@ -1,14 +1,11 @@
-#
-# Variables
-#
 
 export EDITOR=vim
 export VISUAL=subl
 
 # rbenv
+if [ -d "$HOME/.rbenv" ] ; then
 export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
+fi
 
 [ -s $HOME/.nvm/nvm.sh ] && . $HOME/.nvm/nvm.sh  # This loads NVM
-
-# vim: set ft=zsh:
