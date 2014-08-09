29cd1ee5561a788d3341984895dd07351e08e2a9
Julio Napurí <julionc@gmail.com>
2014-08-08 22:34:11 -0500

    More OSX cask packages

--- osx/cask.sh
+++ osx/cask.sh
@@ -5,13 +5,14 @@ brew tap phinze/homebrew-cask
 brew install brew-cask
 
 # Programming
+brew cask install github
 brew cask install iterm2
 brew cask install macvim
-brew cask install sublime-text
 brew cask install postgres
 brew cask install pgadmin3
 brew cask install robomongo
 brew cask install sourcetree
+brew cask install sublime-text
 
 # Virtualization
 brew cask install virtualbox
@@ -24,12 +25,15 @@ brew cask install evernote
 # Everything Else
 brew cask install caffeine
 brew cask install dropbox
+brew cask install dash
 brew cask install google-chrome
 brew cask install jdownloader
 brew cask install transmission
 brew cask install rdio
 brew cask install rescuetime
 brew cask install skype
+brew cask install spotify
+brew cask install steam
 brew cask install vlc
 
 # Games
