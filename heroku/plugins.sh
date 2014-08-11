30766e2a2b2973ec2103549cdb4cfbe8d2ed0f7b
Julio Napurí <julionc@gmail.com>
2014-08-11 18:55:31 -0500

    Heroku

--- heroku/plugins.sh
+++ heroku/plugins.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 function heroku_install() {
-	heroku plugins:install "git://github.com/$1.git"
+	heroku plugins:install "http://github.com/$1.git"
 }
 
 # Plugins
 heroku_install 'tpope/heroku-fucking-console'
 heroku_install 'tpope/heroku-pgbackups-pull'
+heroku_install 'tpope/heroku-accounts' #take care
