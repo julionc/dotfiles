3d349b141d40941317f72eb0c4c1a187c819813d
Julio Napurí <julionc@gmail.com>
2014-10-06 23:04:28 -0500

    GIT, NVM

--- zsh/conf.d/environment.zsh
+++ zsh/conf.d/environment.zsh
@@ -8,4 +8,8 @@ export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
 fi
 
-[ -s $HOME/.nvm/nvm.sh ] && . $HOME/.nvm/nvm.sh  # This loads NVM
+#[ -s $HOME/.nvm/nvm.sh ] && . $HOME/.nvm/nvm.sh  # This loads NVM
+
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+
