11aea2133354012b9af9ab84de8f263e3571cc73
Julio Napurí <julionc@gmail.com>
2015-01-07 15:47:15 -0500

    Add Vagrant

--- vagrant/boxes.sh
+++ vagrant/boxes.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
 # Vagrant Cloud
+
 vagrant box add chef/centos-7.0 --provider virtualbox
-vagrant box add  ubuntu/trusty64 --provider virtualbox
+vagrant box add ubuntu/trusty64 --provider virtualbox
