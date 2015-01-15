920456fb5e6ea13d169f459075ed8cd7a073ab6f
Julio Napurí <julionc@gmail.com>
2015-01-15 12:59:07 -0500

    Zprezto initialize

---
+++ install.sh
@@ -0,0 +1,7 @@
+ln -sf ~/.dotfiles/zprezto/zlogin ~/.zlogin
+ln -sf ~/.dotfiles/zprezto/zlogout ~/.zlogout
+ln -sf ~/.dotfiles/zprezto/zpreztorc ~/.zpreztorc
+ln -sf ~/.dotfiles/zprezto/zprofile ~/.zprofile
+ln -sf ~/.dotfiles/zprezto/zshenv ~/.zshenv
+#ln -sf ~/.dotfiles/zprezto/zshrc ~/.zshrc
+ln -sf ~/.dotfiles/zsh/zshrc ~/.zshrc
--- zsh/conf.d/environment.zsh
+++ zsh/conf.d/environment.zsh
@@ -4,12 +4,18 @@ export VISUAL=subl
 
 # rbenv
 if [ -d "$HOME/.rbenv" ] ; then
-export PATH="$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init -)"
+	export PATH="$HOME/.rbenv/bin:$PATH"
+	eval "$(rbenv init -)"
 fi
+
+if [[ -s "$HOME/.nvm/nvm.sh" ]]; then
+	source "$HOME/.nvm/nvm.sh"
+	export NVM_DIR=~/.nvm
+fi
+
 
 #[ -s $HOME/.nvm/nvm.sh ] && . $HOME/.nvm/nvm.sh  # This loads NVM
 
-source $(brew --prefix nvm)/nvm.sh
-export NVM_DIR=~/.nvm
+#source $(brew --prefix nvm)/nvm.sh
+#export NVM_DIR=~/.nvm
 
