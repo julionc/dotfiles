07a8a8b9ae583ba10a79bcffc9ac4a54b4c4c811
Julio Napurí <julionc@gmail.com>
2015-02-06 11:27:33 -0500

    Add make links

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
+# ZSH
 ln -sf ~/.dotfiles/zprezto/zlogin ~/.zlogin
 ln -sf ~/.dotfiles/zprezto/zlogout ~/.zlogout
 ln -sf ~/.dotfiles/zprezto/zpreztorc ~/.zpreztorc
 ln -sf ~/.dotfiles/zprezto/zprofile ~/.zprofile
 ln -sf ~/.dotfiles/zprezto/zshenv ~/.zshenv
-#ln -sf ~/.dotfiles/zprezto/zshrc ~/.zshrc
 ln -sf ~/.dotfiles/zsh/zshrc ~/.zshrc
+
+# Git
+ln -sf ~/.dotfiles/gitconfig ~/.gitconfig
+ln -sf ~/.dotfiles/gitignore_global ~/.gitignore_global
