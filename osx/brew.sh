1440bce8eab9f8ab1302252247647452ea8132a0
Julio Napurí <julionc@gmail.com>
2016-03-15 21:48:57 -0500

    wrk

--- osx/brew.sh
+++ osx/brew.sh
@@ -32,6 +32,7 @@ brew install the_silver_searcher
 brew install tree
 brew install vim
 #brew install wireshark --with-qt
+brew install wrk
 brew install zsh
 
 # Install Binary apps with homebrew-binary
