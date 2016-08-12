8db0b97c0354ad62eb80754148af0b1d512e985a
Julio Napurí <julionc@gmail.com>
2016-08-12 11:53:53 -0500

    Add Pip aliases

--- aliases.d/python.zsh
+++ aliases.d/python.zsh
@@ -9,4 +9,10 @@ alias web='python -m SimpleHTTPServer'
 alias pypath='python -c "import sys, pprint; pprint.pprint(sys.path)"'
 
 # virtualenv {{{1
+alias workon='source venv/bin/activate'
 alias workoff=deactivate
+
+# pip
+alias pipi='pip install'
+alias pipx='pip uninstall'
+alias pip-remove-all='pip freeze | xargs pip uninstall -y'
