769bbec6bf29083869f0104e3babdc1292aad13d
Julio Napurí <julionc@gmail.com>
2016-08-19 17:23:43 -0500

    [osx] Disable gamed daemon

--- osx/settings.sh
+++ osx/settings.sh
@@ -1,5 +1,3 @@
 # disable `gamed` daemon
 launchctl unload /System/Library/LaunchAgents/com.apple.gamed.plist
-
-# see
-# https://github.com/onanypoint/OSX-Setup/blob/a9b6c6354e171d4f80534a1ab17102945d11085c/roles/system/tasks/services.yml
+defaults write com.apple.gamed Disabled -bool true
