22a2ce4c9750d333b547988b5b5543e611d220d6
Julio Napurí <julionc@gmail.com>
2016-08-21 21:30:32 -0500

    [osx] made ksfetch chrome daemon - check once a week rather than every few hours

--- osx/settings.sh
+++ osx/settings.sh
@@ -1,3 +1,6 @@
 # disable `gamed` daemon
 launchctl unload /System/Library/LaunchAgents/com.apple.gamed.plist
 defaults write com.apple.gamed Disabled -bool true
+
+# Keystone Fetch / ksfetch - once a week
+defaults write com.google.Keystone.Agent checkInterval 604800
