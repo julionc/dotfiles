620f63c965b4eef5f83643579d08a4a87e4faa8b
Julio Napurí <julionc@gmail.com>
2016-12-28 23:04:25 -0500

    Add phpunit-report-runtime

--- aliases.d/php.zsh
+++ aliases.d/php.zsh
@@ -4,3 +4,11 @@ alias phpunit='vendor/bin/phpunit'
 alias phps='php -S localhost:8000'
 alias composeri='composer install'
 alias composeru='composer update'
+
+alias phpunit-report-runtime="phpunit --log-json php://stdout \
+  | awk '\$NF ~ '/,/' && \$1 ~ /\"(test|time)\"/' \
+  | cut -d: -f2- \
+  | sed \"N;s/\n/--/\"  \
+  | sed \"s/,//\"   \
+  | awk 'BEGIN{FS=\"--\"}; {print \$2 \$1}' | sort -r \
+  | head -n 5"
