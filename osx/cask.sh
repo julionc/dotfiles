f997dc59f4d153dc7e7c875bb82ee839ebc688d8
Julio Napurí <julionc@gmail.com>
2018-05-21 18:39:43 -0500

    Update osx cask and java stuff

--- osx/cask.sh
+++ osx/cask.sh
@@ -1,55 +1,36 @@
 #!/usr/bin/env bash
 
-# Music
-brew cask install spotify
-brew cask install spotifree
-
 # Programming
 brew cask install github
 brew cask install iterm2
 brew cask install macvim
 brew cask install phpstorm
-#brew cask install postgres
-#brew cask install pgadmin3
-#brew cask install robomongo
 brew cask install rubymine
 brew cask install sourcetree
-#brew cask install sublime-text
 brew cask install sequel-pro
 brew cask install visual-studio-code
 
 # Productivity
 brew cask install evernote
-#brew cask install gitter
 brew cask install rescuetime
 brew cask install slack
-#brew cask install spectacle
 
 # Everything Else
-#brew cask install adobe-creative-cloud
+brew cask install adobe-creative-cloud
 brew cask install caffeine
 brew cask install cyberduck
 brew cask install dropbox
-brew cask install google-chrome
-brew cask install gyazo
-brew cask install jdownloader
-brew cask install little-snitch
-brew cask install poedit
-brew cask install skype
+brew cask install jumpshare
 brew cask install skitch
+brew cask install spotify
 brew cask install the-unarchiver
 brew cask install transmission
 brew cask install tunnelbear
 
-# Games
-#brew cask install steam
-brew cask install minecraft
-#brew cask install teeworlds
-
 # Virtualization
 brew cask install virtualbox
 brew cask install vagrant
-#brew cask install chefdk
+# brew cask install chefdk
 
 # Remove outdated versions from the Cellar
 brew cleanup
--- osx/java.sh
+++ osx/java.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
 # Java
-brew cask install java7
+# brew cask install java7
 brew cask install java #8
 brew install jenv
 brew cask install intellij-idea-ce
-
-# JavaFX
-brew cask install javafx-scene-builder
+brew cask install eclipse-java
+brew cask install eclipse-jee
 
 # Android
 brew cask install android-sdk
