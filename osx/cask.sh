2494ed3d1515fd2f37c0daf8b27f1dfa38adcbbb
Julio Napurí <julionc@gmail.com>
2018-06-13 18:27:11 -0500

    Update brew cask packages

--- osx/cask.sh
+++ osx/cask.sh
@@ -1,36 +1,43 @@
 #!/usr/bin/env bash
 
 # Programming
+brew cask install datagrip
 brew cask install github
+brew cask install insomnia
 brew cask install iterm2
 brew cask install macvim
 brew cask install phpstorm
+brew cask install pycharm
 brew cask install rubymine
 brew cask install sourcetree
 brew cask install sequel-pro
+brew cask install sourcetree
 brew cask install visual-studio-code
 
 # Productivity
 brew cask install evernote
+brew cask install iterm2
 brew cask install rescuetime
 brew cask install slack
 
 # Everything Else
 brew cask install adobe-creative-cloud
+brew cask install android-file-transfer
 brew cask install caffeine
 brew cask install cyberduck
 brew cask install dropbox
 brew cask install jumpshare
 brew cask install skitch
+brew cask install microsoft-office
 brew cask install spotify
 brew cask install the-unarchiver
 brew cask install transmission
 brew cask install tunnelbear
 
 # Virtualization
+brew cask install vagrant-manager
 brew cask install virtualbox
 brew cask install vagrant
-# brew cask install chefdk
 
 # Remove outdated versions from the Cellar
 brew cleanup
--- osx/java.sh
+++ osx/java.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 # Java
-# brew cask install java7
 brew cask install java #8
 brew install jenv
 brew cask install intellij-idea-ce
 brew cask install eclipse-java
 brew cask install eclipse-jee
+brew cask install sts
 
 # Android
 brew cask install android-sdk
