179febecbf3b16a6252a83a5c8f99fb1713d72f2
Julio Napurí <julionc@gmail.com>
2018-08-03 21:30:23 -0500

    Add postman and cheatsheet to Cask files

--- osx/cask.sh
+++ osx/cask.sh
@@ -8,6 +8,7 @@ brew cask install iterm2
 brew cask install macvim
 brew cask install ngrok
 brew cask install phpstorm
+brew cask install postman
 brew cask install pycharm
 brew cask install rubymine
 brew cask install sourcetree
@@ -25,6 +26,7 @@ brew cask install slack
 brew cask install adobe-creative-cloud
 brew cask install android-file-transfer
 brew cask install caffeine
+brew cask install cheatsheet
 brew cask install cyberduck
 brew cask install dropbox
 brew cask install jumpshare
