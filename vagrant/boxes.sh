c16b9b44cb83468e81ae5676d29df218c52c4318
Julio Napurí <julionc@gmail.com>
2019-06-25 14:24:28 -0500

    Update vagrant stuff

--- vagrant/boxes.sh
+++ vagrant/boxes.sh
@@ -2,7 +2,6 @@
 # Vagrant Cloud
 
 vagrant box add chef/centos-7.0 --provider virtualbox
-# vagrant box add ubuntu/precise64 --provider virtualbox
 vagrant box add ubuntu/trusty64 --provider virtualbox
 vagrant box add ubuntu/xenial64 --provider virtualbox
 vagrant box add ubuntu/bionic64 --provider virtualbox
--- vagrant/plugins.sh
+++ vagrant/plugins.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 # Vagrant plugins
 
-vagrant plugin install vagrant-berkshelf
 vagrant plugin install vagrant-vbguest
 vagrant plugin install vagrant-cachier
