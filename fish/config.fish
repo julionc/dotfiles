9c9647ed674ae556686c3d4de6f218a606db29eb
Julio Napurí <julionc@gmail.com>
2019-12-18 10:04:30 -0500

    [fish] Add abbv and basic funcion

--- fish/config.fish
+++ fish/config.fish
@@ -5,3 +5,11 @@ if test (uname) = 'Darwin'
 	source "$HOME/.config/fish/iterm2_shell_integration.fish"
 	set PATH $PATH "$HOME/.iterm2"
 end
+
+# abbv
+abbr -a gst git status
+abbr -a giff git diff
+abbr -a j z
+
+# StarShip
+starship init fish | source
