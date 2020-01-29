91c678b6520806d61e2f7090828384eb428766c7
Julio Napurí <julionc@gmail.com>
2020-01-29 12:05:02 -0500

    Add youtube-dl abbv to Fish Shell

--- fish/config.fish
+++ fish/config.fish
@@ -12,6 +12,14 @@ abbr -a giff git diff
 abbr -a j z
 abbr -a vim nvim
 abbr -a cat bat
+abbr -a brewi brew install
+abbr -a caski brew cask install
+
+# abbv : youtube-dl
+alias ytd='youtube-dl'
+alias yta='youtube-dl -x --audio-quality 0 '
+alias yt2mp3='youtube-dl -x --audio-format mp3'
+alias yt2mp4='youtube-dl --format mp4 --max-quality mp4'
 
 # StarShip
 starship init fish | source
