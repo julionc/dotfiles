231948191b917976104a85f65d3045330fdc3b01
Julio Napurí <julionc@gmail.com>
2026-09-02 11:35:42 -0500

    git: update fish file

--- config/fish/conf.d/git.fish
+++ config/fish/conf.d/git.fish
@@ -6,14 +6,15 @@ abbr --add --command git --regex co     -- git-co     checkout
 abbr --add --command git --regex st     -- git-st     status -sb
 abbr --add --command git --regex ws     -- git-ws     workspace
 
+# git
+
 abbr -a ga  'git add'
 abbr -a gaa 'git add .'
-abbr -a gc  'git commit -m'
+abbr -a gci 'git commit -m'
 abbr -a gca 'git commit --amend'
 abbr -a gcl 'git clone'
 abbr -a gco 'git checkout'
 abbr -a gcb 'git checkout -b'
-abbr -a gci 'git commit -m'
 abbr -a giff 'git diff'
 abbr -a gds 'git diff --staged'
 abbr -a gl  'git pull'
@@ -26,6 +27,11 @@ abbr -a gss 'git stash'
 abbr -a gsp 'git stash pop'
 abbr -a gsw 'git switch'
 abbr -a gswc 'git switch -c'
+abbr -a grb 'git rebase'
+abbr -a grbi 'git rebase -i'
+abbr -a gl  'git pull'
+abbr -a gp  'git push'
+abbr -a gpf 'git push --force-with-lease'
 abbr -a gt  'git tag'
 abbr -a gtl 'git tag --list'
 abbr -a gpo 'git push origin HEAD'
@@ -33,6 +39,8 @@ abbr -a gpl 'git pull origin HEAD'
 abbr -a grh 'git reset HEAD'
 abbr -a grs 'git restore'
 abbr -a grst 'git restore --staged'
+abbr -a gst  'git status -sb'
+abbr -a gws  'git workspace'
 
 # Logs
 abbr -a glog 'git log --oneline --graph --decorate --all'
